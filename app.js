94266bb4ac384aa8e35b741d949fad9aff2f4b2f
Wfuger <wfuger@gmail.com>
2016-04-01 12:19:05 -0600

    animation from both sides good

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-
 $(function(){
+
   $('#branding').css({backgroundPosition: 'center 40%'});
   $('#content').css({backgroundPosition: 'center -100%'});
   $('#sec-content').css({backgroundPosition: 'center 120%'});
@@ -27,11 +27,33 @@ $(function(){
     setTimeout(bounceHim, 300);
   }
 
+  function startHim2() {
+    num++;
+    $("#sec-content2").animate({top: "-=5px"}, 150).animate({top: "+=5px"}, 150);
+    $("#content2,#branding2").animate({top: "-=" + num + "px"}, 150).animate({top: "+=" + num + "px"}, 150);
+    if (num < 4) {
+      setTimeout(startHim2, 300);
+    } else {
+      setTimeout(bounceHim2, 300);
+    }
+  }
+
+  function bounceHim2() {
+    $("#sec-content2,#branding2").animate({top: "-=5px"}, 150).animate({top: "+=5px"}, 150);
+    $("#content2").animate({top: "-=10px"}, 150).animate({top: "+=10px"}, 150);
+    setTimeout(bounceHim2, 300);
+  }
+
   $('#start').on('click', function() {
     $("#full-robot").animate({
-      left: "50%",
+      left: "5%",
       marginLeft: "400px"
     }, 2000);
+    $("#full-robot2").animate({
+      right: "5%",
+      marginRight: "400px"
+    }, 2000);
     setTimeout(startHim, 2000);
+    setTimeout(startHim2, 2000);
   });
 });
